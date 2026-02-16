4e52c627af668126cbb2f5719f2742bda7681508
CrispyJrIII <plexiversetraveler15@gmail.com>
2026-02-16 08:55:57 -0700

    Successfully got header and footer elements to display

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-dark text-light">
+      <header className="container-fluid">
+        <h1 className="h1">
+          Discount Eden Demo<sup>&reg;</sup>
+        </h1>
+
+        <nav className="navbar navbar-dark">
+          <menu className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="index.html">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="play.html">Play</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="account.html">Account</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="about.html">About</a>
+            </li>
+          </menu>
+        </nav>
+
+        <hr />
+      </header>
+
+      <main>App components go here</main>
+
+      <footer>
+        <hr />
+        <span className="text-reset">Christopher Smith</span>
+        <br />
+        <a className="text-reset" href="https://github.com/CrispyJrIII/startup">GitHub</a>
+      </footer>
+    </div>
+  );
 }
